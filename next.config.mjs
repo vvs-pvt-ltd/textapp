9234fd8f5848c7a5b94fce976dfad46088635a38
Vivek Sahu <viveksahu1762@gmail.com>
2024-05-10 18:56:56 +0530

    Started with PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+import withPWAInit from "@ducanh2912/next-pwa";
+const API_URL = process.env.API_URL;
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development",
+  // disable: false,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_URL}/api/:path*`,
+      },
+    ];
+  },
+};
+
+export default withPWA(nextConfig);
